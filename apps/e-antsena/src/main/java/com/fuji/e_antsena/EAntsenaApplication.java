bdd445d3236eb509ac53e481a6092f0180eea744
Fuji-vanilli <ijufillivan@gmail.co>
2024-10-07 15:06:20 +0000

    update

---
+++ apps/e-antsena/src/main/java/com/fuji/e_antsena/EAntsenaApplication.java
@@ -0,0 +1,13 @@
+package com.fuji.e_antsena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAntsenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAntsenaApplication.class, args);
+	}
+
+}
