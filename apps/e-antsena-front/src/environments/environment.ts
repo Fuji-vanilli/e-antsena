e3f0765f6db0f6a247d0b09e1c83bcac0db2990b
Fuji-vanilli <ijufillivan@gmail.co>
2024-10-11 12:17:33 +0000

    update

---
+++ apps/e-antsena-front/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    kinde: {
+        authority: "https://fuji/kinde.com",
+        redirectUrl: "http://localhost:4200",
+        postLogoutRedirectUri: "http://localhost:4200",
+        clientId: "",
+        audience: "https://fuji.kinde.com/api"
+    },
+    
+    apiUrl: "http://localhost:7000/api"
+}
